95d0e364540941335c7cdf8dc02abe415b5be863
Mia <Mia@MiadeMacBook-Pro.local>
2017-08-09 14:49:01 +0800

    海欣的新闻爬虫

---
+++ haixinSpider/spider.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from  haixinSpider.parseHtml import  *
+import time
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def job():
+    print('发送爬虫存活检测邮件')
+    sender =  SendMail()
+    sender.to_addr = ['wangsheng9297@163.com']
+    sender.sendPlainEmail('如果你在每天11点收到这封邮件，说明勤劳的小爬虫还活着，如果收不到，那小爬虫已经累死了，请电联：18565489297')
+
+
+def detecationSpider():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(job, 'cron', day_of_week='1-7', hour=11, minute=00)
+    scheduler.start()
+
+
+def spider():
+    print('+++++++++++++++++++++++++++++++++++++')
+    print('首次运行爬虫，开始爬取新闻')
+    parser = parseHtml()
+    news = parser.sendAllNewsToEmail()
+    print('本次爬取时间：', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print('本次爬取内容：\n', );
+    print(news)
+    print('+++++++++++++++++++++++++++++++++++++')
+
+    count = 0
+    while True:
+        time.sleep(900)
+        count += 1
+        print('第（%d）次循环运行爬虫，爬取新闻' % count)
+        parser = parseHtml()
+        news = parser.sendAllNewsToEmail()
+        print('本次爬取时间：', time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        print('本次爬取内容：\n', );
+        print(news)
+        print('+++++++++++++++++++++++++++++++++++++')
+
+
+if __name__ == '__main__':
+    # detecationSpider()
+    # spider()
+
+    parser1 = parseHtml()
+    news1 = parser1.getXQRXNews()
+    # news1 = parser1.getStaticHtmlConten('https://mp.weixin.qq.com/profile?src=3&timestamp=1501904762&ver=1&signature=amLuIuerspJwR-eJWmIaX0zlvOQBi012JP-1OFz5Xauv2riQxINqrtMRm4QvCVRl80aTUv944tG6hUaEU67Uww==')
+    print(news1)
+
+
+
+
