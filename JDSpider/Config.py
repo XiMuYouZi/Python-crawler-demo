bd7f65da7ce07f7636c6f8d4f0bd900ff08f26cb
Mia <Mia@MiadeMacBook-Pro.local>
2017-08-13 11:49:34 +0800

    抓取淘宝页面数据

---
+++ JDSpider/Config.py
@@ -0,0 +1,8 @@
+
+MONGO_URL = 'localhost'
+MONGO_DB = 'JD'
+MONGO_TABLE = 'computer'
+
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
+
+KEYWORD = '华硕'
---
+++ JDSpider/Test.py
@@ -0,0 +1,8 @@
+from JDSpider.Spider import *
+
+def main():
+    spider = JDSpider()
+    spider.getComputerInfo()
+
+if __name__ == '__main__':
+    main()
